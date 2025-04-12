64a4168facf3f619787bf13dcbe9c76d089a889a
Austinh1512 <Austinhouchens1510@gmail.com>
2025-04-12 01:56:24 -0400

    add sidebar

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,96 @@
+import { NavLink } from "react-router-dom";
+import {
+  FaHeart,
+  FaHistory,
+  FaRegUserCircle,
+  FaShoppingCart,
+  FaSignOutAlt,
+} from "react-icons/fa";
+
+const Sidebar = () => {
+  return (
+    <div className="p-4 flex flex-col h-full">
+      <div className="mb-6">
+        <h2 className="font-medium text-center my-6 text-3xl">John Doe</h2>
+      </div>
+
+      <nav className="flex flex-col">
+        <div>
+          <p>Buyer</p>
+          <NavLink
+            to="/dashboard/favorites"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-gray-700 text-white rounded flex items-center"
+                : "text-gray-300 hover:bg-gray-700 hover:text-white rounded flex items-center"
+            }
+            style={{
+              padding: "0.75rem 1rem",
+              textDecoration: "none",
+              color: "white",
+              margin: "1rem 0",
+            }}
+          >
+            <FaHeart style={{ marginRight: "0.5rem" }} />
+            Favorites
+          </NavLink>
+          <NavLink
+            to="/dashboard/cart"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-gray-700 text-white rounded flex items-center"
+                : "text-gray-300 hover:bg-gray-700 hover:text-white rounded flex items-center"
+            }
+            style={{
+              padding: "0.75rem 1rem",
+              textDecoration: "none",
+              color: "white",
+              margin: "1rem 0",
+            }}
+          >
+            <FaShoppingCart style={{ marginRight: "0.5rem" }} />
+            Cart
+          </NavLink>
+          <NavLink
+            to="/dashboard/history"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-gray-700 text-white rounded flex items-center"
+                : "text-gray-300 hover:bg-gray-700 hover:text-white rounded flex items-center"
+            }
+            style={{
+              padding: "0.75rem 1rem",
+              textDecoration: "none",
+              color: "white",
+              margin: "1rem 0",
+            }}
+          >
+            <FaHistory style={{ marginRight: "0.5rem" }} />
+            Purchase History
+          </NavLink>
+        </div>
+      </nav>
+
+      <div className="flex flex-col gap-5 mt-auto">
+        <NavLink
+          className="w-full bg-gray-400 hover:bg-gray-500 text-white rounded flex items-center justify-center cursor-pointer"
+          style={{ padding: "0.5rem 1rem" }}
+          to="/dashboard/profile"
+        >
+          <FaRegUserCircle style={{ marginRight: "0.5rem" }} />
+          <span>Manage Profile</span>
+        </NavLink>
+
+        <button
+          className="w-full bg-red-500 hover:bg-red-600 text-white rounded flex items-center justify-center cursor-pointer"
+          style={{ padding: "0.5rem 1rem" }}
+        >
+          <FaSignOutAlt style={{ marginRight: "0.5rem" }} />
+          <span>Sign Out</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
